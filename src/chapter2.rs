7db9b925202607635f78a60012e5b2106614a160
Michael Thaler <michael.thaler@dlr.de>
2020-11-08 14:17:51 +0100

    Updated chapter2.rs again.

--- src/chapter2.rs
+++ src/chapter2.rs
@@ -10,9 +10,18 @@ fn format_result<F>(name: &str, n: i64, f: F) -> String where F: Fn(i64) -> i64 
     format!("The {} of {} is {}", name, n, f(n))
 }
 
+fn find_first<T, P>(v: &Vec<T>, p: P) -> i32 where P: Fn(&T) -> bool {
+    for (i, x) in v.iter().enumerate() {
+        if p(x) {
+            return i as i32;
+        }
+    }
+    -1
+}
+
 #[cfg(test)]
 mod tests {
-    use super::{factorial, format_result};
+    use super::{factorial, format_result, find_first};
 
     #[test]
     fn test_factorial() {
@@ -27,5 +36,12 @@ mod tests {
     #[test]
     fn test_format_result() {
         assert_eq!(format_result("factorial", 5, factorial), "The factorial of 5 is 120");
+        assert_eq!(format_result("absolute value", -5, |n: i64| if n >= 0 { n } else { -n }), "The absolute value of -5 is 5");
+    }
+
+    #[test]
+    fn test_find_first() {
+    
+        assert_eq!(find_first(&vec![1, 10, 100], |x| *x == 10), 1);
     }
 }
